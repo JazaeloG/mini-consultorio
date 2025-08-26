2d5f451f864bbf5833e6b62e6581d1f100bda8e7
JazaeloG <jazaelgalindo.contact@gmail.com>
2025-08-26 16:00:16 -0600

    feature: Initial code for mini consultorio project

---
+++ src/main/java/com/tecnm/miniconsultorio/Miniconsultorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tecnm.miniconsultorio;
+
+/**
+ *
+ * @author Laptop MSI
+ */
+public class Miniconsultorio {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al MiniConsultorio!");
+    }
+}
