098fffa1d609ff00873283b67633ab7b7938ff69
Brayan <bcamarillo138@gmail.com>
2025-09-03 21:50:50 -0600

    Acabo de agregar la clase persona con sus respectivo atributos para ella

---
+++ src/main/java/com/tecnm/miniconsultorio/modelos/PACIENTE.java
@@ -0,0 +1,56 @@
+
+package com.tecnm.miniconsultorio.modelos;
+public class PACIENTE {
+    
+       double altura;
+       int peso;
+       float temperatura;
+       String enfermedades;
+        
+        public PACIENTE (double altura,int peso,float temperatura,String enfermedades){
+            
+            this.altura = altura;
+            this.peso = peso;
+            this.temperatura = temperatura;
+            this.enfermedades = enfermedades;
+            
+        }  
+        
+       public double getaltura(){
+           return altura;
+       }
+       public int getpeso(){
+           return peso;
+       }
+       public float gettemperatura(){
+           return temperatura;
+       }
+       public String getenfermedades(){
+           return enfermedades;
+       }
+       
+       public void setaltura(double altura){
+           this.altura = altura;
+       }
+       public void setpeso(int peso){
+           this.peso = peso;
+       }
+       public void settemperatura(float temperatura){
+           this.temperatura = temperatura;
+       }
+        public void settenfermedades(String enfermedades){
+           this.enfermedades = enfermedades;
+       }
+        
+        public void mostarinformacion(){
+            
+            System.out.println("Informacion del paciente");
+            System.out.println("Su altura es de: "+altura);
+            System.out.println("Su peso es de: "+peso);
+            System.out.println("Su temperatura es de: "+temperatura);
+            System.out.println("Sus emfermedades son las siguintes: "+enfermedades);
+           
+        
+        }
+  }
+  
