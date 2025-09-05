e0ea6a360e0648f3fba6edea10a8c5215b58e194
Rodo <246w0170@zongolica.tecnm.mx>
2025-09-04 21:52:58 -0600

    acabe el modelo enfermera y sus atributos

---
+++ src/main/java/com/tecnm/miniconsultorio/modelos/Enfermera.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tecnm.miniconsultorio.modelos;
+
+/**
+ *
+ * @author rodas
+ */
+public class Enfermera extends Persona{
+   int numeroEnfermera;
+   String areaAtencion;
+}
