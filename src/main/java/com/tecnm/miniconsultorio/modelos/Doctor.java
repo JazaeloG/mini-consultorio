828c2b4789ec96738924f62882ca4fe485a0f247
Julian <josejulian2duran@gmail.com>
2025-09-05 09:29:08 -0600

    agregue clase doctor y atributos y metodos

---
+++ src/main/java/com/tecnm/miniconsultorio/modelos/Doctor.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tecnm.miniconsultorio.modelos;
+
+/**
+ *
+ * @author JULIAN
+ */
+public class Doctor extends Persona{
+    
+    String cedula;
+    String universidadDeEgreso;
+    int anosdeExperiencia;
+    
+    public Doctor(){
+        super();
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getUniversidadDeEgreso() {
+        return universidadDeEgreso;
+    }
+
+    public void setUniversidadDeEgreso(String universidadDeEgreso) {
+        this.universidadDeEgreso = universidadDeEgreso;
+    }
+
+    public int getAnosdeExperiencia() {
+        return anosdeExperiencia;
+    }
+
+    public void setAnosdeExperiencia(int anosdeExperiencia) {
+        this.anosdeExperiencia = anosdeExperiencia;
+    }
+    
+    
+  
+}
