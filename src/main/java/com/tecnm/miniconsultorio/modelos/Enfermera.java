4972e386add02ee3f5c49d0facd8b1eaf8e59bb5
Rodo <246w0170@zongolica.tecnm.mx>
2025-09-06 12:09:21 -0600

    Terminada la clase enfermera con sus metodos

--- src/main/java/com/tecnm/miniconsultorio/modelos/Enfermera.java
+++ src/main/java/com/tecnm/miniconsultorio/modelos/Enfermera.java
@@ -14,12 +14,38 @@ public class Enfermera extends Persona{
     
   private int numeroEnfermera;
    private String areaAtencion;
-   private List<paciente> pacientesAsignados;
+   private List<Paciente> pacientesAsignados;
+
+    public int getNumeroEnfermera() {
+        return numeroEnfermera;
+    }
+
+    public void setNumeroEnfermera(int numeroEnfermera) {
+        this.numeroEnfermera = numeroEnfermera;
+    }
+
+    public String getAreaAtencion() {
+        return areaAtencion;
+    }
+
+    public void setAreaAtencion(String areaAtencion) {
+        this.areaAtencion = areaAtencion;
+    }
+
+    public List<Paciente> getPacientesAsignados() {
+        return pacientesAsignados;
+    }
+
+    public void setPacientesAsignados(List<Paciente> pacientesAsignados) {
+        this.pacientesAsignados = pacientesAsignados;
+    }
+
+    public Enfermera() {
+    }
+
+    public Enfermera(String id, String nombre, int edad) {
+        super(id, nombre, edad);
+    }
    
-   public Enfermera(String nombre, int edad, String id, int numeroEnfermera, String areaAtencion, List<paciente> pasientesAsignados){
-       
-   
-               
-   }
    
 }
